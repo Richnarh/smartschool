1d223ae8114ae19a928a5b52d24f0bbe12d5ce5f
Richard Narh <pascalcppjava@gmail.com>
2022-01-23 01:53:05 +0000

    updates

--- smartweb/src/main/java/com/khoders/smartschool/smartweb/jbeans/controller/StaffAccountController.java
+++ smartweb/src/main/java/com/khoders/smartschool/smartweb/jbeans/controller/StaffAccountController.java
@@ -1,13 +1,77 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package com.khoders.smartschool.smartweb.jbeans.controller;
+
+import com.khoders.resource.jpa.CrudApi;
+import com.khoders.resource.utilities.FormView;
+import com.khoders.smartschool.entities.setup.StaffAccount;
+import java.io.Serializable;
+import java.util.LinkedList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
 
 /**
  *
  * @author richa
  */
-public class StaffAccountController {
+@Named(value = "staffAccountController")
+@SessionScoped
+public class StaffAccountController implements Serializable{
+    @Inject private CrudApi crudApi;
+    private FormView pageView = FormView.listForm();
+    
+    private StaffAccount staffAccount = new StaffAccount();
+    private List<StaffAccount> staffAccountList = new LinkedList<>();
+    
+    private String optionText;
+    
+    @PostConstruct
+    private void init()
+    {
+        
+    }
+    
+    public void initStaff(){
+        
+    }
+    
+    public void saveStaffAccount(){
+        
+    }
+    
+    public void editStaffAccount(StaffAccount staffAccount){
+        
+    }
+    
+    public void deleteStaffAccount(StaffAccount staffAccount){
+        
+    }
+
+    public List<StaffAccount> getStaffAccountList() {
+        return staffAccountList;
+    }
+
+    public FormView getPageView() {
+        return pageView;
+    }
+
+    public void setPageView(FormView pageView) {
+        this.pageView = pageView;
+    }
+
+    public StaffAccount getStaffAccount() {
+        return staffAccount;
+    }
+
+    public void setStaffAccount(StaffAccount staffAccount) {
+        this.staffAccount = staffAccount;
+    }
+
+    public String getOptionText() {
+        return optionText;
+    }
+    
     
 }
